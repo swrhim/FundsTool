1ea0435eee028d0ae6b2dc025a5be6d88f1b187c
swrhim <swrhim@gmail.com>
2014-03-29 17:54:31 -0400

    Sync

--- FundsTool/Model/Payment.cs
+++ FundsTool/Model/Payment.cs
@@ -9,11 +9,10 @@ namespace FundsTool
     public class Payment
     {
         public string Source { get; set; }
-        public int Amount { get; set; }
+        public double Amount { get; set; }
         public string Description { get; set; }
         public DateTime PostDate { get; set; }
         public DateTime TransactionDate { get; set; }
         public string Type { get; set; }
-
     }
 }
---
+++ FundsTool/Utilities/AmexCSVParser.cs
@@ -0,0 +1,52 @@
+﻿using FundsTool.SampleData;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundsTool.Utilities
+{
+    internal class AmexCSVParser : ICSVParser
+    {
+        /// <summary>
+        /// Parse the csv file
+        /// </summary>
+        /// <param name="fileName"></param>
+        public IEnumerable<Payment> ParseFile(string fileName)
+        {
+            try
+            {
+                List<Payment> payments = new List<Payment>();
+                using (var reader = new StreamReader(File.OpenRead(fileName)))
+                {
+                    while(!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        var payment = new Payment();
+
+                        var amount = 0.0;
+
+                        //for now we assume a standard format
+                        var date = Convert.ToDateTime(values[0]);
+                        payment.TransactionDate = date;
+                        var amountString = values[3];
+                        double.TryParse(amountString, out amount);
+                        payment.Amount = amount;
+                        payment.Description = values[2];
+                        payments.Add(payment);
+                    }
+                }
+
+                return payments;
+            }
+            catch (Exception ex)
+            { }
+            
+            return null;
+        }
+    }
+}
